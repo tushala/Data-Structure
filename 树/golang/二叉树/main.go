69f99ee49819624c2fc1f9945924c87bd3dfe38a
tushala <1941603841@qq.com>
2020-03-05 23:38:44 +0800

    差层序遍历

--- 树/golang/二叉树/main.go
+++ 树/golang/二叉树/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val         int
 	left, right *TreeNode
@@ -10,41 +12,85 @@ type Tree struct {
 }
 
 //添加节点
-func (self *Tree) AddNode(val int) {
-	t := &TreeNode{
-		Val:   val,
-		left:  nil,
-		right: nil,
+func add(t *TreeNode, value int) *TreeNode {
+	if t == nil {
+		t = new(TreeNode)
+		t.Val = value
+		return t
 	}
-	p := self.RootNode
-	if p == nil{
-		p = t
-		return
+	if value < t.Val {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
 	}
-	for
+	return t
 }
 
+// 查找最小值
+func (self *Tree) FindMin() int {
+	p := self.RootNode
+	for p.left != nil {
+		p = p.left
+	}
+	return p.Val
+}
+
+// 查找最大值
+func (self *Tree) FindMax() int {
+	p := self.RootNode
+	for p.left != nil {
+		p = p.right
+	}
+	return p.Val
+}
 
 //前序遍历
-func (this *Tree) PreorderTravel() {
+func (self *Tree) PreorderTravel(node *TreeNode) {
+	if node == nil{
+		return
+	}
+	fmt.Print(node.Val,"\t")
+	self.PreorderTravel(node.left)
 
+	self.PreorderTravel(node.right)
 }
 
 //中序遍历
-func (this *Tree) InorderTravel() {
-
+func (self *Tree) InorderTravel(node *TreeNode) {
+	if node == nil{
+		return
+	}
+	self.InorderTravel(node.left)
+	fmt.Print(node.Val,"\t")
+	self.InorderTravel(node.right)
 }
 
 //后序遍历
-func (this *Tree) PostorderTravel() {
-
+func (self *Tree) PostorderTravel(node *TreeNode) {
+	if node == nil{
+		return
+	}
+	self.PostorderTravel(node.left)
+	self.PostorderTravel(node.right)
+	fmt.Print(node.Val,"\t")
 }
 
 //层序遍历
-func (this *Tree) LevelorderTravel() {
+func (self *Tree) LevelorderTravel(node *TreeNode) {
+	if node == nil{
+		return
+	}
+	fmt.Println(node.Val)
 
 }
 
 func main() {
-	tree := Tree{}
-	tree.AddNode(0)
+	tree := &Tree{}
+	insert_slice := []int{17, 5, 34, 2, 11, 35, 29, 38, 9, 16, 8}
+	for _, i := range insert_slice {
+		tree.RootNode = add(tree.RootNode, i)
+	}
+	fmt.Println(tree.FindMin())
+	fmt.Println(tree.FindMax())
+	tree.InorderTravel(tree.RootNode)
+}
