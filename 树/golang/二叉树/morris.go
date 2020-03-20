8819c7d1397586f4f308c95795f60bac316c4af4
tushala <55247481+tushala@users.noreply.github.com>
2020-03-20 23:03:05 +0800

    Update morris.go

--- 树/golang/二叉树/morris.go
+++ 树/golang/二叉树/morris.go
@@ -60,7 +60,63 @@ func (self *Tree) Inorder_Travel(node *TreeNode) {
 
 // 后序遍历
 func (self *Tree) Postorder_Travel(node *TreeNode) {
+	var reverseAndPrint func(node *TreeNode)
+	reverseAndPrint = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
 
+		cur := node
+
+		var b *TreeNode
+		i := 0
+		for cur != nil {
+			p := cur.right
+			if i == 0 {
+				cur.right = nil
+				//if node.Val == 4{
+				//	fmt.Println(11, node.Val,node.right.Val)
+				//	panic("1")
+				//}
+			} else {
+				cur.right = b
+			}
+
+			b = cur
+			cur = p
+			i++
+		}
+		node = b
+		//fmt.Println(123, node)
+		//fmt.Println(456, node.right)
+		for node != nil {
+			fmt.Printf("%d\t", node.Val)
+			node = node.right
+		}
+	}
+
+	if node == nil {
+		return
+	}
+	p := node
+	for p != nil {
+		if p.left != nil {
+			curnode := p.left
+			for curnode.right != nil && curnode.right != p {
+				curnode = curnode.right
+			}
+			if curnode.right == nil {
+				curnode.right = p
+				p = p.left
+				continue
+			} else if curnode.right == p {
+				curnode.right = nil
+				reverseAndPrint(p.left)
+			}
+		}
+		p = p.right
+	}
+	reverseAndPrint(node)
 }
 
 func main() {
@@ -78,4 +134,6 @@ func main() {
 	fmt.Println()
 	//tree.Inorder_Travel(tree.RootNode)
 	//fmt.Println()
+	tree.Postorder_Travel(tree.RootNode)
+	fmt.Println()
 }
