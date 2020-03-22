e34a33075c1c789c1287252bc6ac823ab8b54ea2
tushala <1941603841@qq.com>
2020-03-22 22:45:02 +0800

    查多线程插入

---
+++ 集合/golang/main.go
@@ -0,0 +1,126 @@
+package main
+
+//  https://www.cnblogs.com/golove/p/5840145.html 抄写
+//  用于多例程
+import (
+	"fmt"
+	"sync"
+)
+
+type Set struct {
+	sync.RWMutex
+	m map[int]bool
+}
+
+func New(items ...int) *Set {
+	s := &Set{
+		m: make(map[int]bool, len(items)),
+	}
+	s.Add(items...)
+	return s
+}
+func (self Set) Size() int {
+	self.Lock()
+	defer self.Unlock()
+	return len(self.m)
+}
+
+// 添加元素
+func (self Set) Add(items ...int) {
+	self.Lock()
+	defer self.Unlock()
+	for _, v := range (items) {
+		self.m[v] = true
+	}
+}
+func (self Set) Remove(items ...int) {
+	self.Lock()
+	defer self.Unlock()
+	for _, v := range (items) {
+		delete(self.m, v)
+	}
+}
+func (self *Set) Has(items ...int) bool {
+	self.Lock()
+	defer self.Unlock()
+	for _, v := range items {
+		if _, ok := self.m[v]; !ok {
+			return false
+		}
+	}
+	return true
+}
+func (self *Set) Clear() {
+	self.Lock()
+	defer self.Unlock()
+	self.m = map[int]bool{}
+}
+func (self *Set) IsEmpty() bool {
+	self.Lock()
+	defer self.Unlock()
+	return self.Size() == 0
+}
+// 并集
+func Union(sets ...*Set) *Set{
+	if len(sets) == 0{
+		return New()
+	}else if len(sets) == 1 {
+		return sets[0]
+	}
+	res := sets[0].m
+	for _, set := range sets[1:]{
+		for e := range set.m{
+			if _, ok := res[e]; !ok{
+				res[e] = true
+			}
+			//fmt.Println(123, res)
+		}
+
+	}
+	return &Set{m:res}
+}
+// 差集
+func Minus(sets ...*Set) *Set{
+	if len(sets) == 0{
+		return New()
+	}else if len(sets) == 1 {
+		return sets[0]
+	}
+	res := sets[0].m
+	for _, set := range sets[1:]{
+		for e := range set.m{
+			delete(res, e)
+		}
+	}
+	return &Set{m:res}
+}
+func main() {
+	s1 := New(1, 2, 3, 4, 5, 6, 7, 8)
+	s2 := New(3, 4, 5, 6)
+	s3 := New(1, 2, 5, 6, 8, 9)
+	//r1 := Union(s1, s2, s3)     // 获取并集
+	//r2 := Minus(s1, s2, s3)     // 获取差集
+	//fmt.Println(r1)
+	//fmt.Println(r2)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(n int) {
+			for i := 0; i < 100; i++ {
+				s2.Union(s1) // 获取并集
+				fmt.Printf("%2v：s2 + %v = %v\n", n, s1.SortedList(), s2.SortedList())
+
+				s2.Minus(s3) // 获取差集
+				fmt.Printf("%2v：s2 - %v = %v\n", n, s3.SortedList(), s2.SortedList())
+
+				s2.Intersect(s1) // 获取交集
+				fmt.Printf("%2v：s2 * %v = %v\n", n, s1.SortedList(), s2.SortedList())
+
+				s2.Complement(s1) // 获取 s2 相对于 s1 的补集
+				fmt.Printf("%2v：%v / s2 = %v\n", n, s1.SortedList(), s2.SortedList())
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
