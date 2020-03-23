6987244837adfaf0c5d995183b9b2c36ecab48c4
tushala <1941603841@qq.com>
2020-03-23 23:17:59 +0800

    集合完成

--- 集合/golang/main.go
+++ 集合/golang/main.go
@@ -60,6 +60,54 @@ func (self *Set) IsEmpty() bool {
 	defer self.Unlock()
 	return self.Size() == 0
 }
+func (self *Set) Union(sets ...*Set){
+	// 为了防止多例程死锁，不能同时锁定两个集合
+	// 所以这里没有锁定 s，而是创建了一个临时集合
+	r := self.Duplicate()
+	// 获取并集
+	for _, set := range sets {
+		set.Lock()
+		for e := range set.m {
+			r.m[e] = true
+		}
+		set.Unlock()
+	}
+	// 将结果转入 self
+	self.Lock()
+	defer self.Unlock()
+	self.m = map[int]bool{}
+	for e := range r.m {
+		self.m[e] = true
+	}
+}
+func (self *Set) Minus(sets ...*Set){
+	r := self.Duplicate()
+	for _, set := range sets{
+		set.Lock()
+		for e := range set.m {
+			if _, ok := r.m[e];ok{
+				delete(r.m, e)
+			}
+		}
+		set.Unlock()
+	}
+	// 将结果转入 self
+	self.Lock()
+	defer self.Unlock()
+
+}
+// 创建副本
+func (self *Set) Duplicate()*Set{
+	self.Lock()
+	defer self.Unlock()
+	r := &Set{
+		m: make(map[int]bool, len(self.m)),
+	}
+	for e := range self.m {
+		r.m[e] = true
+	}
+	return r
+}
 // 并集
 func Union(sets ...*Set) *Set{
 	if len(sets) == 0{
@@ -108,16 +156,15 @@ func main() {
 		go func(n int) {
 			for i := 0; i < 100; i++ {
 				s2.Union(s1) // 获取并集
-				fmt.Printf("%2v：s2 + %v = %v\n", n, s1.SortedList(), s2.SortedList())
+				fmt.Printf("%v\n", s2.m)
 
 				s2.Minus(s3) // 获取差集
-				fmt.Printf("%2v：s2 - %v = %v\n", n, s3.SortedList(), s2.SortedList())
-
-				s2.Intersect(s1) // 获取交集
-				fmt.Printf("%2v：s2 * %v = %v\n", n, s1.SortedList(), s2.SortedList())
-
-				s2.Complement(s1) // 获取 s2 相对于 s1 的补集
-				fmt.Printf("%2v：%v / s2 = %v\n", n, s1.SortedList(), s2.SortedList())
+				fmt.Printf("%v\n", s2.m)
+				//s2.Intersect(s1) // 获取交集
+				//fmt.Printf("%2v：s2 * %v = %v\n", n, s1.SortedList(), s2.SortedList())
+				//
+				//s2.Complement(s1) // 获取 s2 相对于 s1 的补集
+				//fmt.Printf("%2v：%v / s2 = %v\n", n, s1.SortedList(), s2.SortedList())
 			}
 			wg.Done()
 		}(i)
